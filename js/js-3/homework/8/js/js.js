aac7a62ef78a85173d67880b1b74b9093faa6372
zys <864967089@qq.com>
2017-11-29 22:47:11 +0800

    1

--- js/js-3/homework/8/js/js.js
+++ js/js-3/homework/8/js/js.js
@@ -1,1 +1,6 @@
-
+function Login() {
+    var n = document.getElementById("name").value;
+    var d = document.getElementById("dept").value;
+    var p = parent.top1.window.document.getElementById("sp");
+    p.innerText= n+","+d;
+}
