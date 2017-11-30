f25d5170f1349040452290eddcd6ce1cf314be8a
qianCheng <864967089@qq.com>
2017-11-30 18:35:09 +0800

    11.30

--- js/js-3/homework/8/js/js.js
+++ js/js-3/homework/8/js/js.js
@@ -1,6 +1,7 @@
 function Login() {
     var n = document.getElementById("name").value;
     var d = document.getElementById("dept").value;
-    var p = parent.top1.window.document.getElementById("sp");
+    // var p = parent.top1.window.document.getElementById("sp");
+    var p = window.parent.frames["top1"].document.getElementById("sp");
     p.innerText= n+","+d;
 }
